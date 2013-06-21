87609f7c99fa864da2c64bf52b021acdbcf68809
Ubuntu <ubuntu@ip-172-31-13-167.us-west-2.compute.internal>
2013-06-21 02:30:19 +0000

    Web app by heroku

--- First_week.js
+++ First_week.js
@@ -1,5 +1,12 @@
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "Modify this script to write out something different.\n";
-fs.writeFileSync(outfile, out);  
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
