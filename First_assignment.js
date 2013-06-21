61112084eff504371555b38e920ff80d65fc4809
Ubuntu <ubuntu@ip-172-31-13-167.us-west-2.compute.internal>
2013-06-21 13:10:25 +0000

    Copy of First_Week1

--- First_assignment.js
+++
@@ -1,5 +0,0 @@
-require('fs');
-var outfile = "hello.txt";
-var out = "Modify this script to write out something different.\n";
-fs.writeFileSync(outfile, out);
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
